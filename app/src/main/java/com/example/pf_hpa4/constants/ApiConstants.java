ed0e188faa4e4660a6439f4fec7dcf49dd471f60
Eric Mason <57864479+ericMaxon@users.noreply.github.com>
2022-12-08 10:11:39 -0500

    minor/update: change attendee status class

--- app/src/main/java/com/example/pf_hpa4/constants/ApiConstants.java
+++ app/src/main/java/com/example/pf_hpa4/constants/ApiConstants.java
@@ -4,12 +4,16 @@ public class ApiConstants {
     public static final String BaseUrl = "https://asistencia-upn43.ondigitalocean.app/api/";
 
     public static class AttendeeStatus {
-        public String present = "Presente";
-        public String late = "Tardanza";
-        public String absent = "Ausencia";
-        public String excusedAbsence = "Ausencia con excusa";
-    }
+        public static int presentId = 1;
 
+        public static String presentName = "Presente";
+        public static int lateId = 2;
+        public String lateName = "Tardanza";
+        public static int absentId = 3;
+        public String absentName = "Ausencia";
+        public static int excusedAbsenceId = 4;
+        public String excusedAbsenceName = "Ausencia con excusa";
+    }
     public static class Roles {
         public static Integer admin = 1;
         public static Integer teacher = 2;
