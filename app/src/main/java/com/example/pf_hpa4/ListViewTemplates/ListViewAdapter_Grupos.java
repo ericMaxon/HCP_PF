15c4b12514f66cd2133287c8f2458e02ef6c18df
Drake AncientDarkFire <bryanchicoloco18@gmail.com>
2022-12-08 17:55:20 -0500

    Funcionamiendo del ListView para ver los grupos

---
+++ app/src/main/java/com/example/pf_hpa4/ListViewTemplates/ListViewAdapter_Grupos.java
@@ -0,0 +1,43 @@
+package com.example.pf_hpa4.ListViewTemplates;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.pf_hpa4.MainActivity;
+import com.example.pf_hpa4.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListViewAdapter_Grupos extends  ArrayAdapter<Grupos>{
+
+    private List<Grupos> opciones = new ArrayList<>();
+
+    public ListViewAdapter_Grupos(Context context, List<Grupos> datos){
+        super(context, R.layout.activity_group_list, datos);
+
+        opciones= datos;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View item = inflater.inflate(R.layout.layout_asignaciones, null);
+
+        TextView lblAsignatura = (TextView)item.findViewById(R.id.txt_asignaciones_asignatura);
+        lblAsignatura.setText("[" + opciones.get(position).getCodigoAsignatura() + "] " + opciones.get(position).getAsignatura());
+
+        TextView lblGrupo = (TextView)item.findViewById(R.id.txt_asignaciones_grupo);
+        lblGrupo.setText(opciones.get(position).getGrupo());
+
+        TextView lblSemestre = (TextView)item.findViewById(R.id.txt_asignaciones_semestre);
+        lblSemestre.setText(opciones.get(position).getPeriodo());
+
+        return(item);
+    }
+
+}
--- app/src/main/java/com/example/pf_hpa4/activities/GroupListActivity.java
+++ app/src/main/java/com/example/pf_hpa4/activities/GroupListActivity.java
@@ -6,11 +6,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.example.pf_hpa4.ListViewTemplates.Grupos;
+import com.example.pf_hpa4.ListViewTemplates.ListViewAdapter_Grupos;
 import com.example.pf_hpa4.R;
 import com.example.pf_hpa4.services.StudentGroupService;
 import com.example.pf_hpa4.services.dto.responses.student.Group;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -18,12 +26,34 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class GroupListActivity extends AppCompatActivity {
+
     int teacherId;
+    ListView Listado_Grupos;
+
+    int idGrupo = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getIntentData(getIntent());
         setContentView(R.layout.activity_group_list);
+
+        Inicializar_controles();
+        LoadListViewTemplate();
+    }
+
+    private void Inicializar_controles(){
+
+        Listado_Grupos = (ListView)findViewById(R.id.group_lvGrupos);
+        Listado_Grupos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                int opcionSeleccionada =
+                        ((Grupos)a.getItemAtPosition(position)).getId();
+
+                idGrupo = opcionSeleccionada;
+                /////cambiar el layaout y llamar al retrofit dependiendo del tipo de usuario
+            }
+        });
     }
 
     private void getIntentData(@NonNull Intent intent) {
@@ -44,4 +74,27 @@ public class GroupListActivity extends AppCompatActivity {
                     });
         }
     }
+
+
+    ///////////Cargar ListView con los grupos
+    private void LoadListViewTemplate()
+    {
+        List<Grupos> opciones = this.GetElementsToListViewGrupos();
+
+        ListViewAdapter_Grupos adapter = new ListViewAdapter_Grupos(this, opciones);
+
+        Listado_Grupos.setAdapter(adapter);
+    }
+
+    private List<Grupos> GetElementsToListViewGrupos()
+    {
+        List<Grupos> opciones = new ArrayList<Grupos>();
+
+        opciones.add(new Grupos(1, "HPA 4", "324", 2, "1IL142", "II Semestre 2022"));
+        opciones.add(new Grupos(2, "Base de Datos", "2341", 2, "1IL112", "II Semestre 2022"));
+
+        return  opciones;
+
+    }
+
 }
